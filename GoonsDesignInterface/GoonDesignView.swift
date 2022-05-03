35d5ae5b5d8355cc00dd287b8799b6810022f962
MJ <terminal201467@gmail.com>
2022-05-03 14:53:51 +0800

    init View and GoonsDesignView

---
+++ GoonsDesignInterface/GoonDesignView.swift
@@ -0,0 +1,20 @@
+//
+//  GoonDesignView.swift
+//  GoonsDesignInterface
+//
+//  Created by Jhen Mu on 2022/5/3.
+//
+
+import UIKit
+
+class GoonDesignView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GoonsDesignInterface/View.swift
@@ -0,0 +1,20 @@
+//
+//  View.swift
+//  GoonsDesignInterface
+//
+//  Created by Jhen Mu on 2022/5/3.
+//
+
+import UIKit
+
+class View: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
